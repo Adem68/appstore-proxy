05cbeb54c93e7e6336492a9b05263c045381990d
Adem Furkan Özcan <ozcanfurkan@gmail.com>
2021-04-04 00:09:50 +0300

    updated

--- app.py
+++ app.py
@@ -15,14 +15,14 @@ app.add_middleware(
 )
 
 @app.get("/app/{app_id}")
-async def proxy(app_id: int):
+def proxy(app_id: int):
     res = requests.get(
         'https://itunes.apple.com/lookup?id={}'.format(app_id)).json()
     return res
 
 
 @app.get("/")
-async def main():
+def main():
     return {
         "message": "404 page not found"
     }
